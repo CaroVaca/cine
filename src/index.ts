37e3710424899f2e60e8f1b053754d15ce671abf
CaroVaca <104107454+CaroVaca@users.noreply.github.com>
2022-05-26 20:06:37 -0300

    acomode la suma acomode unas lineas para que me devuelva la suma de las butacas vacias

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
-//Diseñar un algoritmoque recorra
+//Diseñar un algoritmo que recorra
 //las butacas de una sala de cine
 //y determine cuantas butacas desocupadas hay
-let cantDeButacas: number= Number(prompt("Ingrese la cantidad de butacas que hay en el cine:"));
+let cantDeButacas: number = Number(
+  prompt("Ingrese la cantidad de butacas que hay en el cine:")
+);
 
-let cine: string[] = new Array(cantDeButacas);
-let i: number;
+let cine: boolean[] = new Array(cantDeButacas);
 let butacasDesocupadas: number = 0;
 
-for (i= 0; i<cantDeButacas; i++){
-  cine[i]= Number (prompt("Ingrese true si esta ocupada o  false si no esta ocupada:" + i));
-  } if (cine[i] === "false") {
-       butacasDesocupadas++;
-    }
+for (let i: number = 0; i < cantDeButacas; i++) {
+  cine[i] = Boolean(
+    prompt("La butaca esta ocupada? (true si es si y false si es no)")
+  );
+  if (cine[i] === false) {
+    butacasDesocupadas++;
+  }
 }
-console.log(cine);
 console.log("La cantidad de butacas desocupadas son:" + butacasDesocupadas);
-
